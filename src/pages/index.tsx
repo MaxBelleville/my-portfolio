7468aa5a22050d2714d183eaa8b7cfb089438dbe
MaxBelleville <max.belleville@hotmail.com>
2023-01-26 23:27:24 -0500

    Change colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,23 +13,20 @@ export default function StickyFooter() {
         display: 'flex',
         flexDirection: 'column',
         minHeight: '100vh',
-        color: 'white',
-        backgroundColor: (theme) =>
-        theme.palette.mode === 'light'
-          ? theme.palette.grey[200]
-          : theme.palette.grey[800],
+        color: 'black',
+        backgroundColor: 'white'
       }}
     >
       <CssBaseline />
       <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
+        <Typography variant="h3" component="h1" gutterBottom>
           Sadly, This website is currently being worked on.
         </Typography>
         <Typography variant="h5" component="h2" gutterBottom>
           {'Do not worry it should be done by march 2023'}
           {'If not you can always remind me via an email. Thanks for your patience.'}
         </Typography>
-        <Typography variant="body1"><a href='https://maxbelleville.github.io/old-portfolio/'>Go to the old portfolio.</a></Typography>
+        <Typography variant="body1"><a href='https://maxbelleville.github.io/old-portfolio/'>Go to the old portfolio</a></Typography>
       </Container>
     </Box>
   );
