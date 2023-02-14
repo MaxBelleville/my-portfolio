ead1b084b483b079f6f32d6e12f47cf9ba32bd34
MaxBelleville <max.belleville@hotmail.com>
2023-02-14 01:43:16 -0500

    Change path prefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,6 +6,7 @@ const config: GatsbyConfig = {
     siteUrl: `https://maxbelleville.github.io`,
     pathPrefix: `/my-portfolio`,
   },
+  pathPrefix: `/my-portfolio`,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
